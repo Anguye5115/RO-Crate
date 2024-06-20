2ef920410e7f085352e0d346e40b1c76922ca682
Daniel Wheeler <daniel.wheeler2@gmail.com>
2024-06-20 12:17:37 -0400

    Combined environment for fipy and ro-crate

--- example/build_rocrate.py
+++ example/build_rocrate.py
@@ -2,11 +2,11 @@ from rocrate.rocrate import ROCrate
 from rocrate.model.person import Person
 
 crate = ROCrate()
-yaml = crate.add_file("working/pfhub.yaml", properties={
+yaml = crate.add_file("data/pfhub.yaml", properties={
     "name": "PFHub meta data file",
     "encodingFormat": "text/yaml"
 })
-csv = crate.add_file("working/free_energy_1a.csv", properties={
+csv = crate.add_file("data/free_energy_1a.csv", properties={
     "name": "Free Energy",
     "encodingFormat": "text/csv"
 })
