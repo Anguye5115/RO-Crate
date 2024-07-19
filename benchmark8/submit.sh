721c45aba3e525d05bbaaced051248ff8b8c5fa7
Daniel Wheeler <daniel.wheeler2@gmail.com>
2024-07-19 16:10:31 -0400

    add submit file for cluster

---
+++ benchmark8/submit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=rack1               # -p, first available from the list of partitions
+#SBATCH --time=00-01:00:00                 # -t, time (hh:mm:ss or dd-hh:mm:ss)
+#SBATCH --nodes=1                       # -N, total number of machines
+#SBATCH --ntasks=1                      # -n, 64 MPI ranks per Opteron machine
+#SBATCH --cpus-per-task=1               # threads per MPI rank
+#SBATCH --job-name=benchmark8 # -J, for your records
+#SBATCH --chdir=/working/wd15/surf-2024-austyn-nguyen/benchmark8/cluster   # -D, full path to an existing directory
+#SBATCH --qos=test
+#SBATCH --mem=0G
+#SBATCH --output=slurm-%j.out
+
+omp_threads=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$omp_threads
+export OPENBLAS_NUM_THREADS=$omp_threads
+export MKL_NUM_THREADS=$omp_threads
+export VECLIB_MAXIMUM_THREADS=$omp_threads
+export NUMEXPR_NUM_THREADS=$omp_threads
+
+
+micromamba activate cluster
+python benchmark8a.py params8a.yaml
+
+
